c17530c39c03327b0ce93a8efe20c9588db87d66
nala-he <hytdaisy@gmail.com>
2022-11-16 22:02:45 -0500

    followed instructions and implemented likes button feature

--- src/components/tuits/index.js
+++ src/components/tuits/index.js
@@ -3,7 +3,7 @@ import './tuits.css';
 import Tuit from "./tuit";
 import * as likesService from "../../services/likes-service";
 
-function Tuits({tuits = [], deleteTuit, refreshTuits}) {
+const Tuits = ({tuits = [], deleteTuit, refreshTuits}) => {
     const likeTuit = (tuit) =>
         likesService
             .userTogglesTuitLikes("me", tuit._id)
@@ -14,14 +14,11 @@ function Tuits({tuits = [], deleteTuit, refreshTuits}) {
     <div>
       <ul className="ttr-tuits list-group">
         {
-          tuits.map && tuits.map(tuit => {
-            return (
+          tuits.map(tuit =>
               <Tuit key={tuit._id}
                     deleteTuit={deleteTuit}
                     likeTuit={likeTuit}
-                    tuit={tuit}/>
-            );
-          })
+                    tuit={tuit}/>)
         }
       </ul>
     </div>
